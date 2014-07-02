ed5d3cd631dc02879517ec1d6b18a048392bf663
Lv Zheng <lv.zheng@intel.com>
2014-07-02 16:26:17 +0800

    Add "[PATCH]" generation enhancement.
    This patch adds enhanced generation of "[PATCH]", allowing major/minor
    version number and patch type to be specified.  This patch also sorts help
    messages.
    
    Signed-off-by: Lv Zheng <zetalog@gmail.com>

--- gitformatpatch.sh
+++ gitformatpatch.sh
@@ -1,38 +1,78 @@
 #!/bin/sh
 
+SCRIPT=`(cd \`dirname $0\`; pwd)`
+MSGIDS=$HOME/msgids
+GFPFLAGS="-s --thread=shallow --numbered-files"
+SUFFIX=.patch
+PREFIX=
+NUMBER=1
+COVER=false
+MAJOR=1
+MINOR=0
+MINORSTR=
+TYPE=PATCH
+
 usage()
 {
-	echo "Usage: `basename $0` [-n num] [-cdo] <Message-Id>"
+	echo "Usage: `basename $0`"
+	echo " [-n number] [-c] [-o]"
+	echo " [-p prefix] [-s suffix]"
+	echo " [-r major] [-m minor] [-t type]"
+	echo " [-d] [Message-Id]"
 	echo "Where:"
-	echo "  -c: generate cover letter"
 	echo "  -d: dry run"
+	echo "  -c: generate cover letter"
 	echo "  -n: top most number of patches to generate"
 	echo "  -o: generate outgoing patch with rename/copy detections"
 	echo "  -p: specify patch file prefix"
 	echo "  -s: specify patch file suffix, default is .patch"
+	echo "  -r: specify patch major version, to form [PATCH v<major>], default is 1"
+	echo "  -m: specify patch minor version, to form [PATCH v<major>.<minor>], default is 0"
+	echo "  -t: specify patch type, to form [<type> PATCH]"
 	echo "  Message-Id can be:"
-	echo "    none: for the first message in the thread"
-	echo "    other: Message-Id name in msgids file"
+	echo "    none: for the first message in the thread, this is default for v1, v0.1"
+	echo "    other: Message-Id name in $MSGIDS file"
 	exit 0
 }
 
-SCRIPT=`(cd \`dirname $0\`; pwd)`
-GFPFLAGS="-s -n --thread=shallow"
-MSGIDS=$HOME/msgids
-SUFFIX=.patch
-PREFIX=
-NUMBER=1
+isnumber()
+{
+	if [[ $1 =~ [^0-9] ]]; then
+		return 1
+	fi
+	return 0
+}
 
-while getopts "cdn:op:s:" opt
+fatal()
+{
+	echo
+	echo "Fatal error:"
+	echo $1
+	echo
+	usage
+}
+
+while getopts "cdn:op:s:r:m:t:" opt
 do
 	case $opt in
-	c) GFPFLAGS="$GFPFLAGS --suffix=.patch --cover-letter --numbered-files";;
+	c) COVER=true;;
 	d) DRYRUN="yes";;
-	n) GFPFLAGS="$GFPFLAGS -$OPTARG"
+	n) isnumber $OPTARG || fatal "Argument of -n must be a positive integer: $OPTARG"
+	   if [ $OPTARG -le 0 ]; then
+		fatal "Argument of -n must be greater than 0: $OPTARG"
+	   fi
 	   NUMBER=$OPTARG;;
 	o) GFPFLAGS="$GFPFLAGS -M -C";;
 	p) PREFIX=$OPTARG;;
 	s) SUFFIX=$OPTARG;;
+	r) isnumber $OPTARG || fatal "Argument of -r must be a positive integer: $OPTARG"
+	   MAJOR=$OPTARG;;
+	m) isnumber $OPTARG || fatal "Argument of -m must be a positive integer: $OPTARG"
+	   MINOR=$OPTARG
+	   if [ $MINOR -ne 0 ]; then
+		MINORSTR=".$MINOR"
+	   fi;;
+	t) TYPE="$OPTARG PATCH";;
 	?) echo "Invalid argument $opt"
 	   usage;;
 	esac
@@ -49,14 +89,39 @@ fi
 if [ $NUMBER -ge 1000 ]; then
 	WIDTH=3
 fi
+if [ $NUMBER -ge 2 ]; then
+	GFPFLAGS="$GFPFLAGS -n"
+	if [ "x$COVER" = "xtrue" ]; then
+		GFPFLAGS="$GFPFLAGS --cover-letter"
+	fi
+fi
+GFPFLAGS="$GFPFLAGS -$NUMBER"
 
 if [ "x$1" = "x" ]; then
-	echo "Message-Id is not specified."
-	usage
+	if [ $MAJOR -eq 1 -a $MINOR -eq 0 ]; then
+		fatal "Message-Id is not specified for non v1 series."
+	fi
+	if [ $MAJOR -eq 0 -a $MINOR -eq 1 ]; then
+		fatal "Message-Id is not specified for non v0.1 series."
+	fi
 elif [ "x$1" != "xnone" ]; then
+	if [ $MAJOR -eq 1 -a $MINOR -eq 0 ]; then
+		fatal "Message-Id is specified without specifying >1 version (-r/-m): $1."
+	fi
+	if [ $MAJOR -eq 0 -a $MINOR -eq 1 ]; then
+		fatal "Message-Id $1 is specified without specifying >0.1 version (-r/-m): $1."
+	fi
 	msgid=`cat $MSGIDS | grep $1 | cut -f2`
-	echo $msgid
+	if [ "x$msgid" = "x" ]; then
+		fatal "Cannot find Message-Id in $MSGIDS: $1."
+	fi
+	echo "Found Message-Id: <$msgid>"
 	GFPFLAGS="$GFPFLAGS --in-reply-to=$msgid"
+fi
+if [ $MAJOR -eq 1 -a $MINOR -eq 0 ]; then
+	GFPFLAGS="$GFPFLAGS --subject-prefix='$TYPE'"
+else
+	GFPFLAGS="$GFPFLAGS --subject-prefix='$TYPE v${MAJOR}${MINORSTR}'"
 fi
 
 if [ "x$DRYRUN" != "xyes" ]; then
